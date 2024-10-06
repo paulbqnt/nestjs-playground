b1c7fef61f02f09ca8444b5019ad6617bbd6debc
Paul Boquant <paul.boquant@chartered-investment.com>
2024-10-06 17:18:21 +0200

    feat: add whoami and signout

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-
+const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['asdfasdf']
+  }));
+
   (app as any).set('etag', false);
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
